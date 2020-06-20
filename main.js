55cd0e18951b94756726a6cc89eadcfa2670aeb2
Nonsenses <yarygi66@gmail.com>
2020-06-20 20:41:50 +0300

    sad

--- main.js
+++ main.js
@@ -6,5 +6,11 @@ $(document).ready(function(){
     });
     $('.burger').on('click', function(){
         $('.burger, .navi_mobile').toggleClass('active')
-    })
+    });
+    $('#tab1').on('click', function(){
+        $('.images_tabs, #tab1').toggleClass('active')
+    });
+    setTimeout(function(){
+        $('#tab1').trigger('click');
+      }, 10);
 })
