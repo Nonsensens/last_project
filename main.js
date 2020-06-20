bd272df639b79c43a87dd69274935400ecf714fc
Nonsenses <yarygi66@gmail.com>
2020-06-20 23:05:52 +0300

    dsa

--- main.js
+++ main.js
@@ -7,9 +7,92 @@ $(document).ready(function(){
     $('.burger').on('click', function(){
         $('.burger, .navi_mobile').toggleClass('active')
     });
+    
+
     $('#tab1').on('click', function(){
-        $('.images_tabs, #tab1').toggleClass('active')
+        $('#i_t2, #tab2').removeClass('active')
     });
+    $('#tab1').on('click', function(){
+        $('#i_t3, #tab3').removeClass('active')
+    });
+    $('#tab1').on('click', function(){
+        $('#i_t4, #tab4').removeClass('active')
+    });
+    $('#tab1').on('click', function(){
+        $('#i_t5, #tab5').removeClass('active')
+    });
+
+    $('#tab2').on('click', function(){
+        $('#i_t1, #tab1').removeClass('active')
+    });
+    $('#tab2').on('click', function(){
+        $('#i_t3, #tab3').removeClass('active')
+    });
+    $('#tab2').on('click', function(){
+        $('#i_t4, #tab4').removeClass('active')
+    });
+    $('#tab2').on('click', function(){
+        $('#i_t5, #tab5').removeClass('active')
+    });
+
+    $('#tab3').on('click', function(){
+        $('#i_t2, #tab2').removeClass('active')
+    });
+    $('#tab3').on('click', function(){
+        $('#i_t1, #tab1').removeClass('active')
+    });
+    $('#tab3').on('click', function(){
+        $('#i_t4, #tab4').removeClass('active')
+    });
+    $('#tab3').on('click', function(){
+        $('#i_t5, #tab5').removeClass('active')
+    });
+
+    $('#tab4').on('click', function(){
+        $('#i_t2, #tab2').removeClass('active')
+    });
+    $('#tab4').on('click', function(){
+        $('#i_t3, #tab3').removeClass('active')
+    });
+    $('#tab4').on('click', function(){
+        $('#i_t1, #tab1').removeClass('active')
+    });
+    $('#tab4').on('click', function(){
+        $('#i_t5, #tab5').removeClass('active')
+    });
+
+    $('#tab5').on('click', function(){
+        $('#i_t2, #tab2').removeClass('active')
+    });
+    $('#tab5').on('click', function(){
+        $('#i_t3, #tab3').removeClass('active')
+    });
+    $('#tab5').on('click', function(){
+        $('#i_t4, #tab4').removeClass('active')
+    });
+    $('#tab5').on('click', function(){
+        $('#i_t1, #tab1').removeClass('active')
+    });
+
+
+    $('#tab1').on('click', function(){
+        $('#i_t1, #tab1').toggleClass('active')
+    });
+    $('#tab2').on('click', function(){
+        $('#i_t2, #tab2').toggleClass('active')
+    });
+    $('#tab3').on('click', function(){
+        $('#i_t3, #tab3').toggleClass('active')
+    });
+    $('#tab4').on('click', function(){
+        $('#i_t4, #tab4').toggleClass('active')
+    });
+    $('#tab5').on('click', function(){
+        $('#i_t5, #tab5').toggleClass('active')
+    });
+    
+    
+    
     setTimeout(function(){
         $('#tab1').trigger('click');
       }, 10);
